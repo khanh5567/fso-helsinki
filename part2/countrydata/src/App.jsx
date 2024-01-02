8d84bb7d911ed5adb815ac2549ee92258abb48ac
Jimmy <90267542+khanh5567@users.noreply.github.com>
2024-01-02 10:12:06 -0700

    weather app 2.20

--- part2/countrydata/src/App.jsx
+++ part2/countrydata/src/App.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const Record = ({ country }) => {
+const api_key = import.meta.env.VITE_SOME_KEY;
+
+const Record = ({ country, weather }) => {
   let languages = Object.values(country.languages);
+  if (weather === null) return null;
   return (
     <>
       <h2>{country.name.common}</h2>
@@ -12,8 +15,8 @@ const Record = ({ country }) => {
         <b>languages:</b>
       </p>
       <ul>
-        {languages.map((language) => (
-          <li>{language}</li>
+        {languages.map((language, index) => (
+          <li key={index}>{language}</li>
         ))}
       </ul>
       <img
@@ -21,17 +24,47 @@ const Record = ({ country }) => {
         alt={country.flag}
         style={{ width: "20%" }}
       />
+      <h3>Weather in {country.capital[0]}</h3>
+      <div>temperature {(weather.main.temp - 273.15).toFixed(2)} Celcius</div>
+      <img
+        src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+        alt="Weather Icon"
+      />
+      <div>wind {weather.wind.speed} m/s</div>
     </>
+  );
+};
+
+const ListItem = ({ country, handleClick }) => {
+  return (
+    <div>
+      {country.name.common}{" "}
+      <button onClick={() => handleClick(country.cca3)}>show</button>
+      <br></br>
+    </div>
   );
 };
 
 function App() {
   const [search, setSearch] = useState("");
   const [result, setResult] = useState([]);
+  const [weather, setWeather] = useState(null);
 
   const handleSearch = (event) => {
     setSearch(event.target.value);
   };
+
+  useEffect(() => {
+    if (result.length > 0) {
+      const capital = result[0].capital[0];
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${capital}&unit=imperial&APPID=${api_key}`
+        )
+        .then((res) => setWeather(res.data))
+        .catch((error) => console.log("error: ", error));
+    }
+  }, [result]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -43,9 +76,15 @@ function App() {
             country.name.common.toLowerCase().includes(search.toLowerCase())
           );
           setResult(searchResult);
+          setSearch("");
         })
         .catch((error) => console.log("error occurred"));
     }
+  };
+
+  const handleClick = (id) => {
+    let showCountry = result.filter((country) => country.cca3 === id);
+    setResult(showCountry);
   };
 
   return (
@@ -55,10 +94,18 @@ function App() {
       </form>
       {result.length > 10 ? (
         "Too many matches, try again"
-      ) : result.length == 1 ? (
-        <Record country={result[0]} />
+      ) : result.length === 0 ? (
+        "No matches, try again"
+      ) : result.length === 1 ? (
+        <Record country={result[0]} weather={weather} />
       ) : (
-        result.map((country) => <div>{country.name.common}</div>)
+        result.map((country) => (
+          <ListItem
+            key={country.cca3}
+            country={country}
+            handleClick={handleClick}
+          />
+        ))
       )}
     </>
   );
